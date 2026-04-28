307a09326c6bd53bff97e7a0622ce7a12d43d31b
bartlomiejbialoskorski2004-crypto <bartlomiejbialoskorski2004@gmail.com>
2026-04-28 18:51:34 +0200

    fix: set Polish as the default language

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -10,7 +10,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [language, setLanguage] = useState<Language>('en');
+  const [language, setLanguage] = useState<Language>('pl');
 
   const t = translations[language];
 
